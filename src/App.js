8687a0add20313c456bcdb31e6841dced7dfbbdc
Mohammed Wajid <wajid17023@gmail.com>
2023-07-20 02:13:44 +0530

    adding and styling elements in skills section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Skills from "./Components/Skills";
+
 
 
 function App() {
   return (
     <div >
-      <Navbar/>
-      <Home/>
-      <About/>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
     </div>
   );
 }
